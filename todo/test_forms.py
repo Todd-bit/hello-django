7add9801cfc115908b5c42168fb5c925379c952d
Todd-bit <todd7385@gmail.com>
2021-01-03 17:52:34 +0000

    corrected

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -5,15 +5,15 @@ from .forms import ItemForm
 class TestItemForm(TestCase):
 
     def test_item_name_is_required(self):
-        form = ItemForm({'name':''})
+        form = ItemForm({'name': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('name', form.errors.keys())
         self.assertEqual(form.errors['name'][0], 'This field is required.')
 
     def test_done_field_is_not_required(self):
-        form = ItemForm({'name': 'Test Todo Item'}) 
-        self.assertTrue(form.is_valid()) 
+        form = ItemForm({'name': 'Test Todo Item'})
+        self.assertTrue(form.is_valid())
 
     def test_fields_are_explicit_in_form_metaclass(self):
         form = ItemForm()
-        self.assertEqual(form.Meta.fields, ['name', 'done'])      
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
--- todo/views.py
+++ todo/views.py
@@ -1,40 +1,41 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
-from .forms import Itemform
+from .forms import ItemForm
+
 # Create your views here.
 
 
 def get_todo_list(request):
     items = Item.objects.all()
     context = {
-        'items' : items
+        'items': items
     }
     return render(request, 'todo/todo_list.html', context)
 
 
 def add_item(request):
     if request.method == 'POST':
-        form = Itemform(request.POST)
+        form = ItemForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('get_todo_list')
-    form = Itemform(instance=item)
+    form = ItemForm()
     context = {
-        'form' : form
-    }    
+        'form': form
+    }
     return render(request, 'todo/add_item.html', context)
 
 
 def edit_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     if request.method == 'POST':
-        form = Itemform(request.POST, instance=item)
+        form = ItemForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
             return redirect('get_todo_list')
-    form = Itemform()
+    form = ItemForm(instance=item)
     context = {
-        'form' : form
+        'form': form
     }
     return render(request, 'todo/edit_item.html', context)
 
@@ -45,12 +46,8 @@ def toggle_item(request, item_id):
     item.save()
     return redirect('get_todo_list')
 
+
 def delete_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.delete()
-    return redirect('get_todo_list')    
-    
-
-    
-
-            
+    return redirect('get_todo_list')
